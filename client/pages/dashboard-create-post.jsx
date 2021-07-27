7582abecdf08a7b7660f5848d4c38586e64aeff8
SanB <sandro.brignoli@outlook.fr>
2021-07-27 18:34:07 +0200

    🚨 Test PostForm Build

--- client/pages/dashboard-create-post.jsx
+++ client/pages/dashboard-create-post.jsx
@@ -26,7 +26,7 @@ const dashboardCreatePost = () => {
           textBack='Gestion des articles'
         >
           <Alert />
-          <PostForm id={id} />
+          {/* <PostForm id={id} /> */}
         </Card>
       </Section>
     </div>
